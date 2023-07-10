0b1d68d98172feeffa8f1b38d96a450c41d868be
Thomas-Hummel <thomas.hummel@snhu.edu>
2023-07-10 19:50:59 -0400

    Moved trip information to be data driven from a JSON file

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,10 @@
+/* Get the trip information from a JSON data file */
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways', selected: { travel: true } });
+    res.render('travel', { title: 'Travlr Getaways', selected: { travel: true }, trips });
 };
 
 module.exports = {
